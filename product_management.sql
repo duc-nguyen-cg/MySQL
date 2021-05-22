e53c1147e8085fe2566e5e8d7909e63a3f25bfab
unknown <ngocduc.2109@gmail.com>
2021-05-22 14:15:13 +0700

    [Bài tập] Xây dựng cơ sở dữ liệu quản lý bán hàng

--- product_management.sql
+++ product_management.sql
@@ -5,13 +5,28 @@ use product_management;
 create table customer (
 	cID int not null auto_increment primary key,
 	cName varchar(50) not null,
-    cAge int
+    cAge int not null
 );
 
 create table orders (
 	oID int not null auto_increment primary key,
 	cID int,
 	oDate datetime not null,
-    oTotalPrice int not null,
+    oTotalPrice int ,
     foreign key (cID) references customer(cID)
 );
+
+create table product (
+	pID int not null auto_increment primary key,
+    pName varchar(50) not null,
+    pPrice int not null
+);    
+
+create table orderDetail (
+	oID int not null,
+    pID int not null,
+    odQTY int check (odQTY > 0),
+	unique (oID, pID),
+    foreign key (oID) references orders(oID),
+    foreign key (pID) references product(pID)
+)
